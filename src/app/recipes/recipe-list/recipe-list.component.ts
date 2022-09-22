4244ad951fc1b8a4f348886bc41bb09c051f13be
Rupendra Aathiya <rupendra.aathiya@marutitech.com>
2022-09-22 14:11:25 +0530

    updates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   constructor() { }
+  console = console;
+  recipes:Recipe[] = [
+new Recipe('A test recipe','This is a test','https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706_960_720.jpg')
+  ];
 
   ngOnInit(): void {
   }
