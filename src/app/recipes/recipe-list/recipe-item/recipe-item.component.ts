a49483d90a1bd284a0a7faf0cf0f31571d5b987a
Rupendra Aathiya <rupendra.aathiya@marutitech.com>
2022-11-01 17:52:44 +0530

    routing added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,17 +9,17 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe:Recipe
-  
+  @Input() index:number
 
-  constructor(private recipeService : RecipeService) { }
+  // constructor(private recipeService : RecipeService) { }
 
   ngOnInit(): void {
   }
 
-  onSelected(){
+  // onSelected(){
  
-    this.recipeService.recipeSelected.emit(this.recipe)
+  //   this.recipeService.recipeSelected.emit(this.recipe)
 
-  }
+  // }
 
 }
